8059c5e9e55efa338ce7b0a6bff0ec44e2ce97a1
santhosh414jkh <santhumrsanthumr@gmail.com>
2025-03-12 12:33:43 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,20 +1,8 @@
-
-public class Main {
-    public static void main(String[] args) {
-       float p=6000f;
-       System.out.println("principle amount:"+ p);
-
-       float r=3.3f;
-       System.out.println("rate of interest:"+r);
-
-       float t=2.2f;
-       System.out.println("time taken:"+t);
-
-       float si=(p *r *t)/100;
-       System.out.println("simple interst:"+si);
-
-       
-
-
+class Main
+    {
+        public static void main(String[] args)
+        {
+            System.out.println("hiiiiiiiiiiiii");
+        }
     }
-}
+        
