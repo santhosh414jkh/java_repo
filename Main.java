5f99fbe2178629faf05e83560c49c12bff3e2afa
santhosh414jkh <santhumrsanthumr@gmail.com>
2025-03-12 12:22:22 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+    public static void main(String[] args) {
+       float p=6000f;
+       System.out.println("principle amount:"+ p);
+
+       float r=3.3f;
+       System.out.println("rate of interest:"+r);
+
+       float t=2.2f;
+       System.out.println("time taken:"+t);
+
+       float si=(p *r *t)/100;
+       System.out.println("simple interst:"+si);
+
+       
+
+
+    }
+}
