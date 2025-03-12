eb8c3d53ddf6a8072e2250491667f1c0fed19053
santhosh414jkh <santhumrsanthumr@gmail.com>
2025-03-12 12:18:01 +0530

    Update datatype.java

--- datatype.java
+++ datatype.java
@@ -1,23 +1,6 @@
 public class datatype {
     public static void main(String[] args) {
-        byte rollNo = 43;
-        Short Regno= 3563;
-        String name = "Nuthan";
-        int total = 546;
-        long aadharNo = 68574536989L; 
-        float percentage = 85.32f;
-        double aggregate = 87.21;
-        char grade = 'A'; 
-        boolean eligibility = true;
-
-        System.out.println("Roll No: " + rollNo);
-        System.out.println("Reg No: " + Regno);
-        System.out.println("Name: " + name);
-        System.out.println("Total Marks: " + total);
-        System.out.println("Aadhar No: " + aadharNo);
-        System.out.println("Percentage: " + percentage);
-        System.out.println("Aggregate: " + aggregate);
-        System.out.println("Grade: " + grade);
-        System.out.println("Eligibility: " + eligibility); 
+        
+        System.out.println("Eligibility:"); 
     }
 }
